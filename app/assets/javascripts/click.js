f31023dd4d79d8733691d93430cdae985ca996eb
Ka Mok <heykamok@gmail.com>
2016-05-02 16:38:39 -0400

    Add javascript for owl attributes

--- app/assets/javascripts/click.js
+++ app/assets/javascripts/click.js
@@ -1,6 +1,5 @@
-
 $(function() {
-	$("td").click(function() {
+	$("table#random_table tr td").click(function() {
 		var td = $(this);
 		$.ajax({
 			url: "/get-hex",
@@ -20,17 +19,5 @@ $(function() {
 				console.log("ERROR");
 			}
 		});
-
-		
-	
 	});
 });
-
-//var valid = ["a","b","c","d","e","f","1","2","3","4","5","6","7","8","9"]
-	//var hex = "#"
-
-	//for (i = 0; i < 6; i++) { 
-  // var value = valid[Math.floor(Math.random() * valid.length)];
-  // hex = hex.concat(value)
-  // console.log(hex)
-	//}
