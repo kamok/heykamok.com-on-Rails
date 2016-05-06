4e7255477020c205561a97192417ab5580b7dfe7
Ka Mok <heykamok@gmail.com>
2016-05-06 14:39:54 -0400

    Set up guard and rspec generated. Creating post spec is ready to be tested.

--- spec/features/posts/creating_posts_spec.rb
+++ spec/features/posts/creating_posts_spec.rb
@@ -1,8 +1,15 @@
-require 'spec_helper'
+require 'rails_helper'
 
-feature 'Creating posts' do
+feature 'Creating Posts' do
   scenario 'can create a post' do
-    visit '/'
+    visit '/blog'
 
+    click_link 'New Post'
 
+    fill_in 'Title', with: 'Ka Mok is'
+    fill_in 'Body', with: 'testing a post.'
+    click_button 'Create Post'
+
+    expect(page).to have_content ('Post has been created.')
+  end
 end
