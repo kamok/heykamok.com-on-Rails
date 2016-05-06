9d4d82bb346281f8b4cb2014422380dbd47fcbdc
Ka Mok <heykamok@gmail.com>
2016-05-06 17:24:38 -0400

    Add validation test for title in posts.

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 by [Ka Mok](http://heykamok.com)
 
 Todo :
- 1. Make the blogs url use friendly_id gem 
  2. Add the -RUD in CRUD for post
  3. Get live on Heroku
 
--- spec/features/posts/creating_posts_spec.rb
+++ spec/features/posts/creating_posts_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 feature 'Creating Posts' do
-  scenario 'can create a post' do
+  before do
     visit '/blog'
 
     click_link 'New Post'
+  end
 
+  scenario 'can create a post' do
     fill_in 'Title', with: 'Ka Mok is'
     fill_in 'Body', with: 'testing a post.'
     click_button 'Create Post'
 
     expect(page).to have_content ('Post has been created.')
+  end
+
+  scenario "can not create a project without a name" do
+    click_button 'Create Post'
+
+    expect(page).to have_content("Post has not been created.")
+    expect(page).to have_content("Title can't be blank")
   end
 end
