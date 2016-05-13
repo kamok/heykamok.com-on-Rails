0b7541bb8078eadfa4cc1e98f333b0682c8db34c
Ka Mok <heykamok@gmail.com>
2016-05-13 00:40:09 -0400

    Convert @client_id to string

--- app/models/weatherman.rb
+++ app/models/weatherman.rb
@@ -2,7 +2,7 @@ class Weatherman < ActiveRecord::Base
 
   def self.current_temp
     # ip = get_ip
-    geo_code = ip_to_geo(@client_ip)
+    geo_code = ip_to_geo
     lat = geo_code[0]
     lng = geo_code[1]
     ForecastIO.forecast(lat, lng).currently["temperature"]
@@ -14,8 +14,8 @@ class Weatherman < ActiveRecord::Base
   #   request.remote_ip
   # end
 
-  def self.ip_to_geo(ip)
-    data = open('http://ip-api.com/json/'+ ip ).read
+  def self.ip_to_geo
+    data = open('http://ip-api.com/json/'+ @client_ip.to_s ).read
     lat = JSON.parse(data)["lat"]
     lng = JSON.parse(data)["lon"]
     geo_array = [lat, lng]
