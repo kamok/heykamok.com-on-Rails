733f7acd70548b207c5a94443e4f72b4f66750b4
Ka Mok <heykamok@gmail.com>
2016-05-14 00:53:49 -0400

    Add a conditional for the IP address to use a placeholder(Brazil) when on development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def cache_ip
-    cookies[:client_ip] = "71.125.43.37"#request.remote_ip
+    if Rails.env.production?
+      cookies[:client_ip] = request.remote_ip
+    else 
+      cookies[:client_ip] = "138.0.181.122"
+    end
   end
 end
