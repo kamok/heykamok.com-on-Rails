2fd6ee800631c875af82d3a5d963a24e5f85669c
Ka Mok <heykamok@gmail.com>
2016-05-27 23:49:14 -0400

    Try to add reading list spec

---
+++ spec/features/reading_list/editing_list_spec.rb
@@ -0,0 +1,15 @@
+#You can't create a new reading list om test env
+#And there isnt a reading list in test env in local
+#This test won't pass.
+
+# require 'rails_helper'
+# require 'helpers/posts_helper'
+
+# feature "Editing Reading List" do
+#   before do
+#     sign_in(FactoryGirl.create(:user))
+#   end
+
+#   scenario "Updating the Reading List" do
+#   end
+# end
