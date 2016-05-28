ee67b78ecd7aaba14912ff1b406abc16fa5745dc
Ka Mok <heykamok@gmail.com>
2016-05-28 18:11:10 -0400

    Add ApplicationController specs!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     if Rails.env.production?
       cookies[:client_ip] = request.remote_ip
     else 
-      cookies[:client_ip] = "72.229.28.185"
+      cookies[:client_ip] = "72.229.28.184"
     end
   end
 
--- spec/controllers/application_spec.rb
+++ spec/controllers/application_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 describe ApplicationController do
+
   describe "#cache_ip" do
-    it "saves a cookie called client_ip with an ip address" do
-      expect(subject.cache_ip).to eq("72.229.28.185")
+    it "saves a cookie with the client's ip" do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+      expect(subject.cache_ip).to be_truthy  
+    end
+
+    it "saves a cookie with a default ip address if not in production" do
+      expect(subject.cache_ip).to eq("72.229.28.184")
     end
   end
+
 end
