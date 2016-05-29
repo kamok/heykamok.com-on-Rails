3cf48d9396fa8fe8363d70f6e27a9ec23a1cc95b
Ka Mok <heykamok@gmail.com>
2016-05-28 23:20:19 -0400

    Add prospective tests for Weather

--- spec/models/weatherman_spec.rb
+++ spec/models/weatherman_spec.rb
@@ -10,4 +10,43 @@ describe Weatherman do
       expect(weather.prepare_weather(client_ip)).to be_an(Array)
     end
   end
+
+  describe "#parse_and_store_weather_data" do
+  end
+
+  describe "#prepare_geocode" do
+  end
+
+  describe "#get_weather_data" do
+  end
+
+  describe "#get_current_temp" do
+  end
+
+  describe "#get_current_summary" do
+  end
+
+  describe "#get_day_today" do
+  end
+
+  describe "#get_min_temp_for_day(day)" do
+  end
+
+  describe "#get_max_temp_for_day(day)" do
+  end
+
+  describe "#get_day_for_day(day)" do
+  end
+
+  describe "#get_date_for_day(day)" do
+  end
+
+  describe "#get_icon_for_day(day)" do
+  end
+
+  describe "#translate_icon_verbage(forecast_icon)" do
+  end
+
+  describe "#get_summary_for_day(day)" do
+  end
 end
