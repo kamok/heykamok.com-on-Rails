42fbf025262a0f633b30678571dad5a04fa8db5c
Ka Mok <heykamok@gmail.com>
2016-05-31 20:38:45 -0400

    Comment out unused features on Twilio Controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,21 +7,20 @@ class TwilioController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
-  def voice
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Hey there. You\'ve reached Ka Mok\'s website\'s toy using the Twilio API.', :voice => 'alice'
-        # r.Play 'http://linode.rabasa.com/cantina.mp3'
-        r.Play 'http://picosong.com/Z3qZ/'
-    end
+  # def voice
+  #   response = Twilio::TwiML::Response.new do |r|
+  #     r.Say 'Hey there. You\'ve reached Ka Mok\'s website\'s toy using the Twilio API.', :voice => 'alice'
+  #       r.Play 'http://linode.rabasa.com/cantina.mp3'  ##needs to be a .mp3, no players.
+  #   end
 
-    render_twiml(response)
-  end
+  #   render_twiml(response)
+  # end
 
-  def notify
-    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: '347-933-6917', to: '718-207-7558', body: 'Hey Ka, from Ka.'
-    render plain: message.status
-  end
+  # def notify
+  #   client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+  #   message = client.messages.create from: '347-933-6917', to: '718-207-7558', body: 'Hey Ka, from Ka.'
+  #   render plain: message.status
+  # end
 
   def send_joke
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
