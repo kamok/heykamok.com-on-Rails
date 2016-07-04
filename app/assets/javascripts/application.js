fffacf1d06b60e644608012836ef3094658689cc
Ka Mok <heykamok@gmail.com>
2016-07-03 20:58:27 -0400

    Made tour more pretty

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,17 +19,18 @@
 
 //WYSIWYG
 $(document).ready(function(){
-    $('.wysihtml5').each(function(i, elem) {
-      $(elem).wysihtml5({ toolbar:{ "fa": true, 'html': true} });
-    });
+  $('.wysihtml5').each(function(i, elem) {
+    $(elem).wysihtml5({ toolbar:{ "fa": true, 'html': true} });
+  });
 	// Instance the tour
 	var tour = new Tour({
 	  steps: [
-	  {
-	    element: "#bootstrap-tour-selector",
-	    title: "Greetings Visitor",
-	    content: "You can use these nav buttons on the left to navigate down this homepage with style."
-	  }]
+	  {	element: "#bootstrap-tour-selector",
+	    title: "A word for first time visitors",
+	    content: "You can use these buttons on the left to scroll down this page."
+	  }],
+	  template: 
+	  "<div class='popover tour'><div class='arrow'></div><h3 class='popover-title' style='font-weight:bold;'></h3><div class='popover-content'></div><button class='btn btn-default' style='margin-left:14px; margin-bottom:5px;' data-role='end'>Okay.</button></div>",
 	});
 
 	// Initialize the tour
