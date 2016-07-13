ae661846ef95a7b7815302db25cba5acbc9bc92f
Ka_Lenovo <heykamok@gmail.com>
2016-07-13 17:39:02 -0400

    Comment out the spinner and disable the artificial 0.5 sec delay on the preloader to get rid of a harmless error in the Console.

--- app/assets/javascripts/preloader.js
+++ app/assets/javascripts/preloader.js
@@ -1,9 +1,19 @@
-// wtf is wrong with this function!!!
+// This function which implements a 0.5 secods delay returns an error
+// Doesn't affect functionality but it annoys me
+// I'll skip the delay and simpy get rid of the preload spinner
 
-$(document).ready(function () {
-    $(window).load(setTimeout(function () {
+// $(function () {
+//     $(window).load(setTimeout(function () {
+//         $('#preloader').fadeOut('slow', function() {
+//             $(this).remove();
+//         });
+//     }, 500));
+// });
+
+$(function () {
+    $(window).load(function () {
         $('#preloader').fadeOut('slow', function() {
             $(this).remove();
         });
-    }, 500));
+    });
 });
