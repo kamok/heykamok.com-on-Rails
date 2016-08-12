32668b2b18dd92bdfcbd017cf139b565f41404ba
Ka Mok <heykamok@gmail.com>
2016-08-11 23:42:52 -0400

    Add dates in hash

--- app/controllers/weatherman_controller.rb
+++ app/controllers/weatherman_controller.rb
@@ -1,9 +1,9 @@
 class WeathermanController < ApplicationController
   def get_weather
     weatherman = Weatherman.new
-    custom_weather_data = weather.prepare_forecast(params[:location_query])
-
-    puts custom_weather_data
-    # render json: custom_weather_data
+    custom_weather_data = weather.prepare_forecast("10038")
+    # custom_weather_data = weather.prepare_forecast(params[:location_query])
+    
+    render json: custom_weather_data
   end
 end
--- app/models/weatherman.rb
+++ app/models/weatherman.rb
@@ -4,13 +4,13 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #disable this in production
 
 class Weatherman < ActiveRecord::Base
-  attr_reader :query, :lat, :lng, :forecast_metadata, :location
+  attr_reader :query, :lat, :lng, :location
 
   def prepare_forecast(query)
     @query = query
     prepare_geo_data
     get_weather_data
-
+    build_forecast
   end
 
   private
@@ -24,19 +24,39 @@ class Weatherman < ActiveRecord::Base
 
   def get_weather_data
     @forecast_metadata = ForecastIO.forecast(lat, lng)
+    get_dates
   end
 
   def build_forecast
     forecast = {}
-    forecast["7-days-min"] = build_min
-    forecast["7-days-max"] = build_max
-    forecast["current_temp"] = 
+    forecast["location"] = location
+    forecast["current_temp"] = get_current_temp
+    7.times do |i|
+      forecast["day#{i}"] = {
+        "date" => @dates[i]
+        # "day" => "@dates[i]",
+        # "max" => "@dates[i]",
+        # "min" => "@dates[i]",
+        # "summary" => "@dates[i]",
+        # "icon" => "@dates[i]"
+      }
+    end
+    forecast
   end
 
-  def build_min
+  def get_current_temp
+    @forecast_metadata.currently["temperature"]
   end
 
-  def build_max
+  def get_dates
+    @dates = []
+    7.times { |i| @dates << Time.at(@forecast_metadata.daily.data[i]["time"]).strftime("%-m/%-d") }
   end
+
+  # def build_min
+  # end
+
+  # def build_max
+  # end
 
 end
