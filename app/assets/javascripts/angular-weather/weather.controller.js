91ccae6ca168bb2cc8656795b2c9fe0f3c1d5751
Ka Mok <heykamok@gmail.com>
2016-08-13 19:27:56 -0400

    Add $scope.testing for carousel

--- app/assets/javascripts/angular-weather/weather.controller.js
+++ app/assets/javascripts/angular-weather/weather.controller.js
@@ -13,6 +13,26 @@ function($scope, weatherService) {
 			$scope.todayMax = Math.round(weatherData.weekly_forecast.day0.max);
 			$scope.todayMin = Math.round(weatherData.weekly_forecast.day0.min);
 			$scope.forecast = weatherData.weekly_forecast;
+			
+			var carousel = {}
+			carousel["set1"] = {}
+			carousel["set2"] = {}
+
+			var set1 = Object.keys(weatherData.weekly_forecast).slice(0, 4)
+			for (i in set1) {
+				var key = String(set1[i]);
+				var value = weatherData.weekly_forecast[key];
+				carousel["set1"][key] = value;
+			};
+
+			var set2 = Object.keys(weatherData.weekly_forecast).slice(4, 8)
+			for (i in set2) {
+				var key = String(set2[i]);
+				var value = weatherData.weekly_forecast[key];
+				carousel["set2"][key] = value;
+			};
+
+			$scope.testing = carousel
 		});
 	};
 
