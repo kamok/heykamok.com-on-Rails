b362de0de658e706522eae6ee1e0a8e9bb4444ca
Ka Mok <heykamok@gmail.com>
2016-08-15 00:24:18 -0400

    Fix a bug with git merge choosing the wrong file for me.

--- app/assets/javascripts/angular-weather/weather.controller.js
+++ app/assets/javascripts/angular-weather/weather.controller.js
@@ -12,32 +12,7 @@ function($scope, weatherService) {
 			$scope.currentSummary = weatherData.current_summary;
 			$scope.todayMax = Math.round(weatherData.weekly_forecast.day0.max);
 			$scope.todayMin = Math.round(weatherData.weekly_forecast.day0.min);
-			// $scope.forecast = weatherData.weekly_forecast;
-			
-			var carousel = {}
-
-			carousel["set1"] = {}
-			carousel["set2"] = {}
-
-			var set1 = Object.keys(weatherData.weekly_forecast).slice(0, 4)
-			for (i in set1) {
-				var key = String(set1[i]);
-				var value = weatherData.weekly_forecast[key];
-				carousel["set1"][key] = value;
-			};
-			
-			var set2 = Object.keys(weatherData.weekly_forecast).slice(4, 8)
-			for (i in set2) {
-				var key = String(set2[i]);
-				var value = weatherData.weekly_forecast[key];
-				carousel["set2"][key] = value;
-			};
-
-			carousel["set1"]["id"] = "0"
-			carousel["set2"]["id"] = "1" 
-			$scope.slides = carousel
-
-
+			$scope.forecast = weatherData.weekly_forecast;
 		});
 	};
 
