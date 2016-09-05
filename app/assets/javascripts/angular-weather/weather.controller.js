21f9c751d38f6d2cfa6db74b65cb2c70a968e2cb
Ka Mok <heykamok@gmail.com>
2016-09-05 16:21:40 -0400

    Fix css, tidy old assets

--- app/assets/javascripts/angular-weather/weather.controller.js
+++ app/assets/javascripts/angular-weather/weather.controller.js
@@ -23,20 +23,5 @@ function($scope, weatherService) {
 
   $scope.active = 0;
 
-  // var slides = $scope.slides = [];
-
-  // $scope.addSlide = function() {
-  //   var newWidth = 600 + slides.length + 1;
-  //   slides.push({
-  //     image: 'http://lorempixel.com/' + newWidth + '/300',
-  //     text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-  //     id: currIndex++
-  //   });
-  // };
-
-  // for (var i = 0; i < 4; i++) {
-  //   $scope.addSlide();
-  // }
-
 fetchWeather("Central Park");
 }]);
